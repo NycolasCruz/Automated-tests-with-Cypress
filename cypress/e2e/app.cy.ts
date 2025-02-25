4ea6361c2ce8bfb66d2336dcf68e7aee4e7542a2
nycolascruz <nycolascruzsilva@gmail.com>
2025-02-25 16:59:10 -0300

    test: App testing in progress

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -1,9 +1,16 @@
+// descrever melhor o que o teste faz, em inglês
+
 describe("página inicial", () => {
 	before(() => {
 		cy.visit("http://localhost:8000/");
 	});
 
-	it("deve renderizar o texto correto", () => {
-		cy.dataCy("new-item-label").contains("Adicione um novo item à lista");
+	it("deve clicar adicionar Pokémons novos à lista e verificar se a mensagem de feedback aparece", () => {
+		cy.dataCy("add-pokémons-button").click();
+		cy.dataCy("status-message")
+			.should("exist")
+			.and("have.text", "Pokémons adicionados com sucesso!");
+		// cy.dataCy("status-message").should("have.text", "Pokémons adicionados com sucesso!");
+		// Assertions
 	});
 });
