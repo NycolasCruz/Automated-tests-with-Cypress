57b1388236ce5b8bd4f79e7116746b8de69426d2
NycolasCruz <nycolascruzsilva@gmail.com>
2025-04-13 17:10:45 -0300

    chore: cy config

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,8 +4,5 @@ export default defineConfig({
 	e2e: {
 		defaultBrowser: "edge",
 		baseUrl: "http://localhost:8000",
-		// setupNodeEvents(on, config) {
-		// implement node event listeners here
-		// },
 	},
 });
