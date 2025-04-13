dc9cbad5d5762db0900eace04e74025f5bbabeb3
NycolasCruz <nycolascruzsilva@gmail.com>
2025-04-13 17:23:34 -0300

    test: dividindo os testes em novos cenários

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -3,19 +3,21 @@ describe("manipulando itens", () => {
 		cy.visit("");
 	});
 
-	it("deve verificar se o botão de adicionar está inicialmente desabilitado, adicionar Pokémons novos à lista, verificar se a mensagem de feedback aparece e está correta e verificar se o formulário foi limpo ao final", () => {
-		cy.dataCy("add-pokémons-button").should("be.disabled");
+	it("deve verificar se o botão de adicionar está inicialmente desabilitado", () => {
+		cy.dataCy("add-pokemons-button").should("be.disabled");
+	});
 
-		cy.dataCy("new-pokémons-input").type("Pikachu");
-		cy.dataCy("add-pokémons-button").click();
+	it("deve adicionar Pokémons novos à lista, verificar se a mensagem de feedback aparece e está correta e verificar se o formulário foi limpo ao final", () => {
+		cy.dataCy("new-pokemons-input").type("Pikachu");
+		cy.dataCy("add-pokemons-button").click();
 
 		cy.dataCy("status-message").should("have.text", "Pokémon adicionado com sucesso!");
 
-		cy.dataCy("new-pokémons-input").should("have.value", "");
+		cy.dataCy("new-pokemons-input").should("have.value", "");
 	});
 
 	it("deve adicionar Pokémons aleatórios à lista e verificar se a mensagem de feedback aparece e está correta", () => {
-		cy.dataCy("add-random-pokémons-button").click();
+		cy.dataCy("add-random-pokemons-button").click();
 		cy.dataCy("status-message").should("have.text", "Pokémons adicionados com sucesso!");
 	});
 });
