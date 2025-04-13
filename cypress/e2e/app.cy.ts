a1dbfbdba0b6c4e4ff98c0e89e272f06c40e6525
NycolasCruz <nycolascruzsilva@gmail.com>
2025-04-13 17:10:31 -0300

    test: testes finalizados

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -3,7 +3,18 @@ describe("manipulando itens", () => {
 		cy.visit("");
 	});
 
-	it("deve clicar adicionar Pokémons novos à lista e verificar se a mensagem de feedback aparece", () => {
+	it("deve verificar se o botão de adicionar está inicialmente desabilitado, adicionar Pokémons novos à lista, verificar se a mensagem de feedback aparece e está correta e verificar se o formulário foi limpo ao final", () => {
+		cy.dataCy("add-pokémons-button").should("be.disabled");
+
+		cy.dataCy("new-pokémons-input").type("Pikachu");
+		cy.dataCy("add-pokémons-button").click();
+
+		cy.dataCy("status-message").should("have.text", "Pokémon adicionado com sucesso!");
+
+		cy.dataCy("new-pokémons-input").should("have.value", "");
+	});
+
+	it("deve adicionar Pokémons aleatórios à lista e verificar se a mensagem de feedback aparece e está correta", () => {
 		cy.dataCy("add-random-pokémons-button").click();
 		cy.dataCy("status-message").should("have.text", "Pokémons adicionados com sucesso!");
 	});
